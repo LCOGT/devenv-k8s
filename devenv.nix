54903a23ea5ae13452c5123a0e6d665cd8330edf
Jashandeep Sohi <jsohi@lco.global>
2023-12-01 14:51:04 -0800

    add skaffold custom build script for buildx

--- devenv.nix
+++ devenv.nix
@@ -18,5 +18,21 @@ in
     octopilot
   ];
 
+  scripts.skaffold-builder-buildx.exec = ''
+    set -ex
+
+    args=""
+
+    if test "$PUSH_IMAGE" = true; then
+      args+="--push "
+    fi
+
+    if test -n "$PLATFORMS"; then
+      args+="--platform $PLATFORMS "
+    fi
+
+    docker buildx build "$BUILD_CONTEXT" --tag $IMAGE $args $SKAFFOLD_BUILDX_ARGS
+  '';
+
   # See full reference at https://devenv.sh/reference/options/
 }
