b9815abb993fa10288193e2f2ed2ff2708cb4fb5
Jashandeep Sohi <95394050+jashan-lco@users.noreply.github.com>
2024-12-18 11:18:45 -0800

    remove cd update scripts

--- cd.nix
+++ cd.nix
@@ -32,52 +32,5 @@
       kustomize build prod/ --output output/prod/manifest.yaml
       popd
     '';
-
-    scripts.cd-update-staging.exec = ''
-      set -ex
-
-      if test -z "$1"; then
-        echo "first argument should be an absolute path to the skaffold build output"
-        exit 1
-      fi
-
-      if test -z "$2"; then
-        echo "second argument should be the git commit hash"
-        exit 1
-      fi
-
-      pushd $DEVENV_ROOT
-
-      pushd staging/
-
-      kpt pkg update --strategy force-delete-replace base@$2
-
-      if test -f "$1"; then
-        pushd cd-set-images
-        sh -xe <(cat $1 | jq -r '.builds[] | "kustomize edit set image \(.imageName)=\(.tag)"')
-        popd
-      fi
-      popd
-
-      kustomize build staging/ --output output/staging/manifest.yaml
-      popd
-    '';
-
-    scripts.cd-update-prod.exec = ''
-      set -ex
-
-      if test -z "$1"; then
-        echo "first argument should be git commit hash of staging"
-        exit 1
-      fi
-
-      pushd $DEVENV_ROOT
-
-      kpt pkg update --strategy force-delete-replace prod/base@$2
-      kpt pkg update --strategy force-delete-replace prod/cd-set-images@$2
-
-      kustomize build prod/ --output output/prod/manifest.yaml
-      popd
-    '';
   };
 }
